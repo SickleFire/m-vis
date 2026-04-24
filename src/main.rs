a35ff80e769f566cb3f2e83e203a826f0e35267d
SickleFire <sicklefirex@gmail.com>
2026-04-24 22:18:17 +0800

    added application name input

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,18 @@ use windows::Win32::System::Memory::{
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let query = &args[1];
-    
 
     match query.as_str() {
         "scan" => {
-            let querypid = &args[2];
-            let pid: u32 = querypid.parse().unwrap();
+            let queryp = &args[2];
+            let pid = find_pid(queryp.to_string()).expect("failed to find process");
             let mode = &args[3];
             scan_with_modes(mode, pid);
         }
         "--help" => {
             println!("commands");
-            println!("scan [pid] [modes]");
+            println!("scan [app.exe] [modes]");
             println!("--help");
             println!("");
             println!("modes");
@@ -36,6 +34,19 @@ fn main() {
             println!("Invalid Command: {}", query);
         }
     }
+}
+
+fn find_pid(name: String) -> Result<u32, String>{
+    use sysinfo::System;
+    let mut sys = System::new_all();
+    sys.refresh_all();
+    for (pid, process) in sys.processes() {
+        if *process.name() == *name {
+            let process_id = pid.as_u32();
+            return Ok(process_id);
+        }
+    }
+    Err("Can't find process name".to_string())
 }
 
 fn scan_with_modes(mode: &String, pid: u32){
