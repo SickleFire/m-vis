f2d442d42422ac73edfb4005d61ed9f61d3d136a
SickleFire <sicklefirex@gmail.com>
2026-09-15 23:18:14 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
+use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
+    println!("cargo:rerun-if-changed=tests/test_programs/leak_test_c.c");
+
     let src = PathBuf::from("tests/test_programs/leak_test_c.c");
-    let out = PathBuf::from("tests/test_programs/leak_test_c.exe");
+    
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
+    let out = out_dir.join("leak_test_c.exe");
 
     if !out.exists() {
         let status = Command::new("gcc")
